8d2cbb48016b07c5e4b280f1c6cfeb24b5e81a65
Ava <61526317+a-powelson@users.noreply.github.com>
2025-10-06 12:03:58 -0300

    init - detection agent

---
+++ host_agent/host_agent.ebpf.c
@@ -0,0 +1,319 @@
+#include <net/sock.h>
+#include <net/inet_sock.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+#include <linux/tcp.h>
+#include <linux/pkt_cls.h>
+#include <linux/bpf.h>
+#include <linux/sched.h>
+
+struct fid
+{
+  u32 address;
+  u16 port;
+  u8 proto;
+};
+
+struct flowStat
+{
+  u32 size;
+  u64 arrival;
+};
+
+struct data_out
+{
+  u32 init_addr;
+  u16 init_port;
+  u8 init_proto;
+  int init_sz;
+  u64 init_tm;
+  u32 res_addr;
+  u16 res_port;
+  u8 res_proto;
+  int res_sz;
+  u64 res_tm;
+};
+
+// This is the BCC default size for hashmaps
+#define DEFAULT_HASH_SIZE 10240
+
+BPF_RINGBUF_OUTPUT(output, 1);
+BPF_HASH("lru_hash", struct fid, struct flowStat, flowStats, DEFAULT_HASH_SIZE);
+BPF_HASH("lru_hash", u16, struct fid, inPorts, DEFAULT_HASH_SIZE);
+BPF_HASH("lru_hash", u32, struct fid, pids, DEFAULT_HASH_SIZE);
+BPF_HASH("lru_hash", u16, struct fid, outPorts, DEFAULT_HASH_SIZE);
+BPF_HASH("lru_hash", u32, struct sock*, currSock, DEFAULT_HASH_SIZE);
+
+/*
+ PROCESS TRACING
+*/
+int kretprobe____x64_sys_execve(struct pt_regs *ctx)
+{
+  struct task_struct *task;
+  struct fid *fid;
+
+  u32 pid = bpf_get_current_pid_tgid() >> 32;
+
+  fid = pids.lookup(&pid);
+  if (fid != 0)
+  {
+    pids.update(&pid, fid);
+  }
+
+  return 0;
+}
+
+int kretprobe____x64_sys_clone(struct pt_regs *ctx)
+{
+  u32 child_pid = PT_REGS_RC(ctx);
+  if (child_pid <= 0)
+  {
+    return 1;
+  }
+
+  u32 pid = bpf_get_current_pid_tgid() >> 32;
+  struct fid *fid = pids.lookup(&pid);
+  if (fid != 0)
+  {
+    pids.update(&child_pid, fid);
+  }
+
+  return 0;
+}
+
+TRACEPOINT_PROBE(sched, sched_process_exit)
+{
+  struct task_struct *task = (typeof(task))bpf_get_current_task();
+
+  if (task != 0)
+  {
+    u32 pid = task->tgid;
+    if (pid != 0)
+      pids.delete(&pid);
+  }
+  return 0;
+}
+
+/*
+ INCOMING TCP ACCESS
+*/
+int kretprobe__inet_csk_accept(struct pt_regs *ctx)
+{
+  struct sock *newsk = (struct sock *)PT_REGS_RC(ctx);
+  u32 pid = bpf_get_current_pid_tgid() >> 32;
+
+  if (newsk == NULL)
+    return 0;
+
+  u16 dport = newsk->__sk_common.skc_dport; // remote port (i.e., client src)
+  dport = ntohs(dport);
+
+  struct fid *fid;
+
+  fid = inPorts.lookup(&dport);
+  if (fid != 0 && pid != 0)
+  {
+    pids.update(&pid, fid);
+  }
+
+  return 0;
+}
+
+/*
+ OUTGOING TCP ACCESS
+*/
+int kprobe__tcp_v4_connect(struct pt_regs *ctx, struct sock *sk)
+{
+  u32 pid = bpf_get_current_pid_tgid() >> 32;
+  currSock.update(&pid, &sk);
+  return 0;
+};
+
+int kretprobe__inet_hash_connect(struct pt_regs *ctx)
+{
+  u32 pid = bpf_get_current_pid_tgid() >> 32;
+
+  // check if connection was successful
+  int ret = PT_REGS_RC(ctx);
+  if (ret != 0)
+  {
+    currSock.delete(&pid);
+    return 0;
+  }
+
+  struct sock **skpp;
+  skpp = currSock.lookup(&pid);
+  if (skpp == 0)
+  {
+    return 0; // missed entry
+  }
+
+  struct sock *skp = *skpp;
+  struct inet_sock *sockp = (struct inet_sock *)skp;
+  u16 sport = sockp->inet_sport;
+  sport = ntohs(sport);
+
+  struct fid *fid;
+
+  fid = pids.lookup(&pid);
+  if (fid != 0)
+  {
+    outPorts.update(&sport, fid);
+  }
+
+  currSock.delete(&pid);
+
+  return 0;
+}
+
+/*
+ INGRESS
+*/
+#define IP_TCP 6
+
+int handle_ingress(struct xdp_md *ctx)
+{
+  void *data_end = (void *)(long)ctx->data_end;
+  void *data = (void *)(long)ctx->data;
+  struct ethhdr *eth = data;
+  struct iphdr *iph;
+  struct tcphdr *tcph;
+  struct fid fid = {};
+  struct flowStat *fstat;
+
+  if (data + sizeof(*eth) > data_end)
+    return XDP_PASS;
+
+  if (bpf_ntohs(eth->h_proto) != ETH_P_IP)
+    return XDP_PASS;
+
+  if (data + sizeof(*eth) + sizeof(*iph) > data_end)
+    return XDP_PASS;
+
+  iph = data + sizeof(*eth);
+
+  if (iph->protocol == IP_TCP)
+  {
+    if (data + sizeof(*eth) + sizeof(*iph) + sizeof(*tcph) > data_end)
+      return XDP_PASS;
+
+    tcph = data + sizeof(*eth) + sizeof(*iph);
+
+    fid.address = iph->saddr ^ iph->daddr;
+    fid.port = tcph->source ^ tcph->dest;
+    fid.proto = iph->protocol;
+
+    fstat = flowStats.lookup(&fid);
+
+    if (fstat == NULL)
+    {
+      struct flowStat newStat;
+      newStat.size = bpf_ntohs(iph->tot_len);
+      newStat.arrival = bpf_ktime_get_ns();
+      flowStats.insert(&fid, &newStat);
+    }
+    else
+    {
+      fstat->size += bpf_ntohs(iph->tot_len);
+    }
+
+    u16 tcp_src = ntohs(tcph->source);
+
+    if (tcp_src != 0)
+    {
+      inPorts.update(&tcp_src, &fid);
+    }
+
+    return XDP_PASS;
+  }
+  return XDP_PASS;
+}
+
+/*
+ EGRESS
+*/
+int handle_egress(struct __sk_buff *skb)
+{
+  void *data_end = (void *)(long)skb->data_end;
+  void *data = (void *)(long)skb->data;
+  struct ethhdr *eth = data;
+  struct iphdr *iph;
+  struct tcphdr *tcph;
+  struct fid *in_fid;
+  struct fid fid = {};
+  struct flowStat *fstat;
+
+  if (data + sizeof(*eth) > data_end)
+    return TC_ACT_OK;
+
+  if (bpf_ntohs(eth->h_proto) != ETH_P_IP)
+    return TC_ACT_OK;
+
+  if (data + sizeof(*eth) + sizeof(*iph) > data_end)
+    return TC_ACT_OK;
+
+  iph = data + sizeof(*eth);
+
+  if (iph->protocol == IP_TCP)
+  {
+    if (data + sizeof(*eth) + sizeof(*iph) + sizeof(*tcph) > data_end)
+      return TC_ACT_OK;
+
+    tcph = data + sizeof(*eth) + sizeof(*iph);
+
+    fid.address = iph->saddr ^ iph->daddr;
+    fid.port = tcph->source ^ tcph->dest;
+    fid.proto = iph->protocol;
+
+    fstat = flowStats.lookup(&fid);
+
+    if (fstat == NULL)
+    {
+      struct flowStat newStat;
+      newStat.size = bpf_ntohs(iph->tot_len);
+      newStat.arrival = bpf_ktime_get_ns();
+      flowStats.insert(&fid, &newStat);
+    }
+    else
+    {
+      fstat->size += bpf_ntohs(iph->tot_len);
+    }
+
+    u16 sport = ntohs(tcph->source);
+    in_fid = outPorts.lookup(&sport);
+
+    if (in_fid != 0)
+    {
+      struct flowStat *in_stats = flowStats.lookup(in_fid);
+      struct flowStat *curr_stats = flowStats.lookup(&fid);
+
+      if (in_stats != NULL && curr_stats != NULL &&
+          in_fid->address != fid.address &&
+          in_stats->arrival > curr_stats->arrival - 1000000000 &&
+          abs(in_stats->size - curr_stats->size) < 6500)
+      {
+        struct data_out data = {};
+
+        data.init_addr = in_fid->address;
+        data.init_port = in_fid->port;
+        data.init_proto = in_fid->proto;
+        data.init_sz = in_stats->size;
+        data.init_tm = in_stats->arrival;
+
+        data.res_addr = iph->daddr;
+        data.res_port = bpf_ntohs(tcph->dest);
+        data.res_proto = iph->protocol;
+        data.res_sz = curr_stats->size;
+        data.res_tm = curr_stats->arrival;
+
+        output.ringbuf_output(&data, sizeof(data), 0);
+        // bpf_trace_printk("Instigator ID: %u, %u, %u", in_fid->address, in_fid->port, in_fid->proto);
+      }
+    }
+
+    return TC_ACT_OK;
+  }
+
+  return TC_ACT_OK;
+}
---
+++ host_agent/host_agent.py
@@ -0,0 +1,73 @@
+from bcc import BPF
+import socket
+from ctypes import *
+import pyroute2
+from pyroute2 import NetlinkError
+import re
+
+device = 'enp101s0f1np1'
+dev2 = "eno1"
+
+print("USING NICs " + device + ", " + dev2)
+ipr = pyroute2.IPRoute()
+eth = ipr.link_lookup(ifname=device)[0]
+eth2 = ipr.link_lookup(ifname=dev2)[0]
+
+try:
+    ipr.tc("add", "clsact", eth)
+except NetlinkError as e:
+    if 'File exists' in str(e):
+        pass
+    else:
+        raise
+
+try:
+    ipr.tc("add", "clsact", eth2)
+except NetlinkError as e:
+    if 'File exists' in str(e):
+        pass
+    else:
+        raise
+
+b = BPF(src_file="host_agent.ebpf.c")
+
+fn = b.load_func("handle_ingress", BPF.XDP)
+b.attach_xdp(device, fn, 0)
+b.attach_xdp(dev2, fn, 0)
+
+f_egress = b.load_func("handle_egress", BPF.SCHED_CLS)
+ipr.tc("add-filter", "bpf", eth, ':1', fd=f_egress.fd, name=f_egress.name,
+        parent='ffff:fff3', classid=1, direct_action=True)
+ipr.tc("add-filter", "bpf", eth2, ':1', fd=f_egress.fd, name=f_egress.name,
+        parent='ffff:fff3', classid=1, direct_action=True)
+
+def unsignedToSigned(n, byte_count): 
+  return int.from_bytes(n.to_bytes(byte_count, 'little', signed=False), 'little', signed=True)
+
+def bitwise_xor_bytes(a, b):
+    result_int = int.from_bytes(a, byteorder="big") ^ int.from_bytes(b, byteorder="big")
+    return result_int.to_bytes(4, byteorder="big")
+
+def print_event(cpu, data, size):
+    data = b["output"].event(data)
+    ip_xor = data.init_addr.to_bytes(4, 'little')
+    eno1 = socket.inet_aton("10.50.1.2")
+    res_ip = socket.inet_ntoa(bitwise_xor_bytes(ip_xor, eno1))
+    if(re.match("10.50.1.*", res_ip)):
+        print(f"INIT FID -> SADDR:{res_ip}, PORT:{data.init_port}, PROTO:{data.init_proto}, SZ:{data.init_sz}, T:{unsignedToSigned(data.init_tm, 8)}", end=" ")
+    else:
+        print(f"INIT FID -> SADDR:{ip_xor}, PORT:{data.init_port}, PROTO:{data.init_proto}, SZ:{data.init_sz}, T:{unsignedToSigned(data.init_tm, 8)}", end=" ")
+
+    print(f"RESULT -> DADDR:{socket.inet_ntoa(data.res_addr.to_bytes(4, 'little'))}, DPORT:{data.res_port}, PROTO:{data.res_proto}, SZ:{data.res_sz}, T:{unsignedToSigned(data.res_tm, 8)}")
+
+b["output"].open_ring_buffer(print_event)
+print("Setup complete. Listening for events...")
+while 1:
+    try:
+        b.ring_buffer_poll()
+    except KeyboardInterrupt:
+        b.remove_xdp(device, 0)
+        ipr.tc("del-filter", "bpf", eth, ':1', protocol=0, prio=0xc000, parent='ffff:fff3')
+        b.remove_xdp(dev2, 0)
+        ipr.tc("del-filter", "bpf", eth2, ':1', protocol=0, prio=0xc000, parent='ffff:fff3')
+        exit()
